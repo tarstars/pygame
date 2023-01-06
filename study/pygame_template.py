248c34b57160a0967e2f45a93ff87907e55380fc
tarstars <142857@mai.ru>
2023-01-06 15:03:32 +0300

    template for pygame study projects

---
+++ study/pygame_template.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+def main():
+    shape = width, height = (1200, 800)
+    pygame.init()
+    screen = pygame.display.set_mode(shape)
+    running = True
+
+    clock = pygame.time.Clock()
+    while running:
+        clock.tick(40)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 170, 0))
+        pygame.display.update()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
