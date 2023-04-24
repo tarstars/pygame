a38378e50d4aaa51599d0510272ab3ec5fc96980
tarstars <142857@mai.ru>
2023-04-24 20:33:11 +0300

    sun movement fixed

--- study/sea.py
+++ study/sea.py
@@ -1,9 +1,30 @@
+import math
+import pygame
 import random
 
 
+def draw_sun(screen, local_meter, y):
+    n = 17
+    r = 200
+    pygame.draw.circle(surface=screen, color=(250, 250, 0), center=(local_meter, y), radius=100)
 
 
-import pygame
+def draw_scene(screen, width, height, meter):
+    local_meter = (meter * 5) % width
+    pygame.draw.rect(surface=screen, color=(0, 0, 0), rect=(0, 0, 100, 70))
+
+    draw_sun(screen, local_meter, 100)
+
+    for _ in range(30):
+        pygame.draw.arc(surface=screen, color=(0, 255, 255),
+                        rect=(random.randint(10, width), random.randint(10, height), 70, 70), start_angle=0,
+                        stop_angle=3.14,
+                        width=3)
+    for _ in range(30):
+        pygame.draw.arc(surface=screen, color=(255, 255, 255),
+                        rect=(random.randint(10, width), random.randint(10, height), 90, 80), start_angle=4.16,
+                        stop_angle=3.14,
+                        width=8)
 
 
 def main():
@@ -13,21 +34,15 @@ def main():
     running = True
 
     clock = pygame.time.Clock()
+    meter = 0
     while running:
-        clock.tick(3)
+        clock.tick(10)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 128, 255))
-        pygame.draw.rect(surface=screen, color=(0, 0, 0), rect=(0, 0, 100, 70))
-        for times in range(20, 1000, 10):
-            pygame.draw.circle(surface=screen, color=(250, 250, 0), center=(times, 100), radius=100)
-        for _ in range(30):
-            pygame.draw.arc(surface=screen, color=(0, 255, 255), rect=(random.randint(10, width), random.randint(10, height), 70, 70), start_angle=0, stop_angle=3.14,
-                            width=3)
-        for _ in range(30):
-            pygame.draw.arc(surface=screen, color=(255, 255, 255), rect=(random.randint(10, width), random.randint(10, height), 90, 80), start_angle=4.16, stop_angle=3.14,
-                            width=8)
+        draw_scene(screen, width, height, meter)
+        meter += 1
         pygame.display.update()
     pygame.quit()
 
