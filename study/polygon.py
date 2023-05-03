460ddbe57d69bb1a03ffcecae1a803a67c872e3b
faripoilol <alinab200483@mail.ru>
2023-05-03 19:37:36 +0300

    images

--- study/polygon.py
+++ study/polygon.py
@@ -14,7 +14,10 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 170, 0))
-        pygame.draw.polygon(surface=screen, color=(250, 0, 250), points=((600, 400), (650, 400), (650, 350)))
+        pygame.draw.circle(surface=screen, color=(0, 128, 255), center=(400, 300), radius=100, width=100)
+        pygame.draw.polygon(surface=screen, color=(250, 0, 250), points=((600, 400), (700, 400), (700, 500), (600, 500)))
+        pygame.draw.polygon(surface=screen, color=(250, 0, 250),
+                            points=((700, 500), (650, 600), (600, 500)))
         pygame.display.update()
     pygame.quit()
 
