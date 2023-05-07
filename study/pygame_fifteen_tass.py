069b2c6266a4e7a7d08bd1026bf9ae7b8316df43
tarstars <142857@mai.ru>
2023-05-07 22:55:39 +0300

    sound for fifteen game

--- study/pygame_fifteen_tass.py
+++ study/pygame_fifteen_tass.py
@@ -8,6 +8,7 @@ def main():
     running = True
 
     numbers = [pygame.image.load(f"../Numbers/{v}.jpg") for v in range(1, 16)]
+    click_sound = pygame.mixer.Sound("../Sounds/metal-short-tick.aiff")
     field = [[14, 1, 2, 3],
              [4, None, 6, 7],
              [8, 9, 10, 11],
@@ -35,6 +36,7 @@ def main():
         if (hole_x, hole_y) != (hole_next_x, hole_next_y) and 0 <= hole_next_x < 4 and 0 <= hole_next_y < 4 :
             field[hole_y][hole_x], field[hole_next_y][hole_next_x] = field[hole_next_y][hole_next_x], field[hole_y][hole_x]
             hole_x, hole_y = hole_next_x, hole_next_y
+            pygame.mixer.Sound.play(click_sound)
 
         screen.fill((0, 170, 0))
 
