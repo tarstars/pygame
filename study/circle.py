2a4a083bb657e1a4dd42e64f8c4d13a3154e89aa
faripoilol <alinab200483@mail.ru>
2023-05-07 16:05:15 +0300

    missing files

--- study/circle.py
+++ study/circle.py
@@ -1,6 +1,23 @@
 import pygame
 
+
 def main():
-    pygame.draw.circle(surface=screen,color=(250,250,0),center=(0,0),radius=(10))
+    shape = width, height = (1200, 800)
+    pygame.init()
+    screen = pygame.display.set_mode(shape)
+    running = True
+
+    clock = pygame.time.Clock()
+    while running:
+        clock.tick(40)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 170, 0))
+        pygame.draw.circle(surface=screen, color=(250, 250, 0), center=(600, 400), radius=100)
+        pygame.display.update()
+    pygame.quit()
+
+
 if __name__ == "__main__":
     main()
--- study/ellipse.py
+++ study/ellipse.py
@@ -2,7 +2,22 @@ import pygame
 
 
 def main():
-pygame.draw.ellipse(surface=screen,color=(0,250,250),rect=rect)
+    shape = width, height = (1200, 800)
+    pygame.init()
+    screen = pygame.display.set_mode(shape)
+    running = True
+
+    clock = pygame.time.Clock()
+    while running:
+        clock.tick(40)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 170, 0))
+        pygame.draw.ellipse(surface=screen, color=(0, 250, 250), rect=(650, 450, 500, 400))
+        pygame.display.update()
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
