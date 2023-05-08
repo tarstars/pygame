a321ce0c116e8b6c294d8fdcc8a9eb68cbb861e9
faripoilol <alinab200483@mail.ru>
2023-05-08 20:11:42 +0300

    15.py

--- study/15.py
+++ study/15.py
@@ -10,11 +10,11 @@ def main():
     numbers = [pygame.image.load(f"../Numbers/{v}.jpg") for v in range(1, 16)]
     click_sound = pygame.mixer.Sound("../Sounds/click.mp3")
     fail_sound = pygame.mixer.Sound("../Sounds/fail.mp3")
-    field = [[0, 1, 2, 3],
+    field = [[10, 13, 12, 3],
              [4, 5, 6, 7],
-             [8, 9, 10, 11],
-             [12, 13, 14, None]]
-
+             [8, 9, 0, 1],
+             [2, 1, 14, None]]
+    state = "game"
     hole_x, hole_y = 3, 3
 
     clock = pygame.time.Clock()
@@ -39,9 +39,9 @@ def main():
             if 0 <= hole_next_x < 4 and 0 <= hole_next_y < 4:
                 field[hole_y][hole_x], field[hole_next_y][hole_next_x] = field[hole_next_y][hole_next_x], field[hole_y][hole_x]
                 hole_x, hole_y = hole_next_x, hole_next_y
-                pygame.mixer.music.play(click_sound)
+                pygame.mixer.Sound.play(click_sound)
             else:
-                pygame.mixer.music.play(fail_sound)
+                pygame.mixer.Sound.play(fail_sound)
         screen.fill((0, 170, 0))
 
         for x in range(4):
