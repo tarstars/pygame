12d9d40c8bce0cc74f240bd6815dcf63ebfa200c
tarstars <142857@mai.ru>
2023-05-08 10:17:01 +0300

    sound for wrong move

--- study/pygame_fifteen_tass.py
+++ study/pygame_fifteen_tass.py
@@ -9,6 +9,8 @@ def main():
 
     numbers = [pygame.image.load(f"../Numbers/{v}.jpg") for v in range(1, 16)]
     click_sound = pygame.mixer.Sound("../Sounds/metal-short-tick.aiff")
+    fart_sound = pygame.mixer.Sound("../Sounds/fart.mp3")
+
     field = [[14, 1, 2, 3],
              [4, None, 6, 7],
              [8, 9, 10, 11],
@@ -33,10 +35,13 @@ def main():
                 elif event.key == pygame.K_DOWN:
                     hole_next_y += 1
 
-        if (hole_x, hole_y) != (hole_next_x, hole_next_y) and 0 <= hole_next_x < 4 and 0 <= hole_next_y < 4 :
-            field[hole_y][hole_x], field[hole_next_y][hole_next_x] = field[hole_next_y][hole_next_x], field[hole_y][hole_x]
-            hole_x, hole_y = hole_next_x, hole_next_y
-            pygame.mixer.Sound.play(click_sound)
+        if (hole_x, hole_y) != (hole_next_x, hole_next_y):
+            if 0 <= hole_next_x < 4 and 0 <= hole_next_y < 4 :
+                field[hole_y][hole_x], field[hole_next_y][hole_next_x] = field[hole_next_y][hole_next_x], field[hole_y][hole_x]
+                hole_x, hole_y = hole_next_x, hole_next_y
+                pygame.mixer.Sound.play(click_sound)
+            else:
+                pygame.mixer.Sound.play(fart_sound)
 
         screen.fill((0, 170, 0))
 
