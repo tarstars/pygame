9122a83023f95cabf1ff598794bc6832db96d438
tarstars <142857@mai.ru>
2023-06-05 20:05:23 +0300

    body

--- study/snake_tass.py
+++ study/snake_tass.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import pygame
 
 
@@ -10,6 +12,7 @@ def main():
     dx, dy = 10, 0
     cx, cy = width // 2, height // 2
     x, y = cx, cy
+    q = deque()
 
     clock = pygame.time.Clock()
     while running:
@@ -28,10 +31,14 @@ def main():
                     dx, dy = 10, 0
 
         x, y = x + dx, y + dy
+        q.append((x, y))
+        if len(q) > 9:
+            q.popleft()
 
         screen.fill((0, 170, 0))
 
-        pygame.draw.circle(surface=screen, color=(255, 0, 0), center=(x, y), radius=10)
+        for pos in q:
+            pygame.draw.circle(surface=screen, color=(255, 0, 0), center=pos, radius=10)
 
         pygame.display.update()
     pygame.quit()
