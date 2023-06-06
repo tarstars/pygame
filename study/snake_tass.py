8eeca49790872c42bbab84e035b8ca4f7eeb1cb2
tarstars <142857@mai.ru>
2023-06-06 16:11:25 +0300

    body with our queue

--- study/snake_tass.py
+++ study/snake_tass.py
@@ -1,6 +1,38 @@
-from collections import deque
-
 import pygame
+
+
+class OurQueue:
+    def __init__(self, n):
+        self.a = [0] * n
+        self.n = len(self.a)
+        self.r = 0
+        self.l = 0
+
+    def push(self, v):
+        self.a[self.r] = v
+        self.r = (self.r + 1) % self.n
+
+    def peek(self):
+        return self.a[self.l]
+
+    def pop(self):
+        self.l = (self.l + 1) % self.n
+
+    def empty(self):
+        return self.r == self.l
+
+    def __iter__(self):
+        def queue_iterator():
+            current_pos = self.l
+            while current_pos != self.r:
+                yield self.a[current_pos]
+                current_pos = (current_pos + 1) % self.n
+        return queue_iterator()
+
+    def __len__(self):
+        if self.l <= self.r:
+            return self.r - self.l
+        return self.n - (self.l - self.r)
 
 
 def main():
@@ -12,11 +44,11 @@ def main():
     dx, dy = 10, 0
     cx, cy = width // 2, height // 2
     x, y = cx, cy
-    q = deque()
+    q = OurQueue(1000)
 
     clock = pygame.time.Clock()
     while running:
-        clock.tick(5)
+        clock.tick(10)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -31,9 +63,9 @@ def main():
                     dx, dy = 10, 0
 
         x, y = x + dx, y + dy
-        q.append((x, y))
-        if len(q) > 9:
-            q.popleft()
+        q.push((x, y))
+        if len(q) > 20:
+            q.pop()
 
         screen.fill((0, 170, 0))
 
