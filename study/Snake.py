5683216d5a303f6ef7d36f40e208e11d806d2f75
faripoilol <alinab200483@mail.ru>
2023-06-06 10:59:10 +0300

    new loose in pencils.py

--- study/Snake.py
+++ study/Snake.py
@@ -6,24 +6,41 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(shape)
     running = True
+    dx = 20
+    dy = 0
+    x = 0
+    y = 0
+    pew_sound = pygame.mixer.Sound("../Sounds/a8887ebd8bd4435.mp3")
 
     clock = pygame.time.Clock()
     while running:
-        clock.tick(40)
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    print("a")
+                    dx = -20
+                    dy = 0
                 elif event.key == pygame.K_w:
-                    print("w")
+                    dx = 0
+                    dy = -20
                 elif event.key == pygame.K_s:
-                    print("s")
+                    dx = 0
+                    dy = 20
                 elif event.key == pygame.K_d:
-                    print("d")
+                    dx = 20
+                    dy = 0
+                elif event.key == pygame.K_SPACE:
+                    pygame.mixer.Sound.play(pew_sound)
+        x, y = x + dx, y + dy
         screen.fill((0, 170, 0))
-        pygame.display.update()
+        pygame.draw.circle(screen, (250, 0, 0), (width // 2 + x, height // 2 + y), 5)
+        pygame.draw.circle(screen, (250, 0, 0), (width // 2 + x, height // 2 + y - 10), 5)
+        pygame.draw.circle(screen, (250, 0, 0), (width // 2 + x, height // 2 + y + 10), 5)
+        pygame.draw.circle(screen, (250, 0, 0), (width // 2 + x - 10, height // 2 + y), 5)
+        pygame.draw.circle(screen, (250, 0, 0), (width // 2 + x + 10, height // 2 + y), 5)
     pygame.quit()
 
 
