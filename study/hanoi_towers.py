b27e95d7cf560fa806a3612e2b8ca59846f5bd38
faripoilol <alinab200483@mail.ru>
2023-07-10 14:56:30 +0300

    hanoi_towers.py

--- study/hanoi_towers.py
+++ study/hanoi_towers.py
@@ -6,10 +6,8 @@ import pygame
 def main():
     img_green_rect = pygame.image.load("../Images/green_rect.png")
     img_red_rect = pygame.image.load("../Images/red_rect.png")
-    gr = 0
-    rr = 0
-    choosed_1 = 0
-    choosed_2 = 0
+    chosen_1 = 0
+    chosen_2 = 0
     a = random.randint(1, 24)
     b = random.randint(1, 24)
     c = random.randint(1, 24)
@@ -33,29 +31,27 @@ def main():
                         state = "red rect"
                     elif state == "red rect":
                         state = "green rect"
-                        rr = 0
-                        gr = 0
                 elif event.key == pygame.K_a:
-                    if state == "green rect":
-                        gr -= 300
-                    elif state == "red rect":
-                        rr -= 300
+                    if state == "green rect" and chosen_1 > 0:
+                        chosen_1 -= 1
+                    elif state == "red rect" and chosen_2 > 0:
+                        chosen_2 -= 1
                 elif event.key == pygame.K_d:
-                    if state == "green rect":
-                        gr += 300
-                    elif state == "red rect":
-                        rr += 300
+                    if state == "green rect" and chosen_1 < 2:
+                        chosen_1 += 1
+                    elif state == "red rect" and chosen_2 < 2:
+                        chosen_2 += 1
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((250, 50, 250))
-        x = width // 2 - 100 + gr
+        x = width // 2 - 100 + chosen_1 * 300
         y = height // 2 - 100
         screen.blit(img_text_1, (width // 2 - 300, height // 2))
         screen.blit(img_text_2, (width // 2, height // 2))
         screen.blit(img_text_3, (width // 2 + 300, height // 2))
         screen.blit(img_green_rect, (x, y))
         if state == "red rect":
-            screen.blit(img_red_rect, (x + rr, y))
+            screen.blit(img_red_rect, (x + chosen_2 * 300, y))
         pygame.display.update()
     pygame.quit()
 
